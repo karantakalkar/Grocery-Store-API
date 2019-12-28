5727e4fef340c07df92229c49da65c7dca723245
karan <karant@iitk.ac.in>
2019-12-28 23:00:09 +0530

    segregated code into models and resources

---
+++ resources/items.py
@@ -0,0 +1,74 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel
+
+class Item(Resource):
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {'message': 'Item not found'}, 404
+
+    def post(self,name):
+        if ItemModel.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)},400
+        parser = reqparse.RequestParser()
+
+        data = Item.parser.parse_args()
+        item = ItemModel(name, data['price'])
+
+        try:
+            item.insert()
+        except:
+            return {"message": "An error occured inserting the item."}, 500
+
+        return item.json(), 201
+
+    def delete(self, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query)
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+        updated_item = ItemModel(name, data['price'])
+
+        if item is None :
+            try:
+                updated_item.insert()
+            except:
+                return {"message": "an error occured inserting the item."}, 500
+        else :
+            try:
+                updated_item.update()
+            except:
+                return {"message": "an error occured updating the item."}, 500
+        return updated_item.json()
+
+
+class ItemList(Resource):
+    def get(self):
+        connection =sqlite3.connect('data.db')
+        cursor.execute(query, (item['name'], item['price']))
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+
+        return {'items': items}
